efb6b2da438e5b1b9279bfd9429683eefba23569
Alim-Oxcytech <alim.mohd@oxcytech.com>
2023-11-02 20:10:34 +0530

    remove table

--- src/components/GoalsAndStrategies/view-goals/subComponents/ListSection.jsx
+++ src/components/GoalsAndStrategies/view-goals/subComponents/ListSection.jsx
@@ -1,13 +1,11 @@
 import { Box, Grid } from "@mui/material";
 import React from "react";
-import EditableTable from "./EditableTable";
 
 const ListSection = () => {
   return (
     <Box sx={{ flexGrow: 1 }} mb={2} mt={2}>
       <Grid container>
         <Grid item xs={12} lg={12}>
-          <EditableTable tableTile={"Created Goals"} />
         </Grid>
       </Grid>
     </Box>
