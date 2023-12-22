4aeea88af73225c83951aafb01d9e7a588478f55
Jameel-Oxcytech <jameel.syed@oxcytech.com>
2023-12-22 10:59:58 +0530

    conflict resolved

--- src/components/Tasks/subComponents/PortfolioListSection.jsx
+++ src/components/Tasks/subComponents/PortfolioListSection.jsx
@@ -1,5 +1,5 @@
 import { Grid } from "@mui/material";
-import React, { useState, useEffect,memo } from "react";
+import React, { useState, useEffect } from "react";
 import TaskTable from "./TaskTable";
 import { useSelector } from "react-redux";
 import { selectUserDetails } from "../../../redux/action/userSlice";
@@ -8,7 +8,8 @@ import Loader from "../../common/Loader";
 import NoListTaskFound from "./NoListTaskFound";
 import MyPagination from "../../common/MyPagination";
 
-const PortfolioListSection = ({ rows, setRows }) => {
+const PortfolioListSection = () => {
+  const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(false);
   const user = useSelector(selectUserDetails);
   // const regId = user?.reg_id;
@@ -49,10 +50,17 @@ const PortfolioListSection = ({ rows, setRows }) => {
   return (
     <Grid container mt={2}>
       <Grid item xs={12}>
-        {loading ? <Loader /> : <TaskTable rows={rows} setRows={setRows} fetchData={fetchData} />}
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <TaskTable rows={rows} setRows={setRows} fetchData={fetchData} />
+            {rows?.length > 0 && <MyPagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />}
+          </>
+        )}
       </Grid>
     </Grid>
   );
 };
 
-export default memo(PortfolioListSection);
+export default PortfolioListSection;
